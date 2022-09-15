b344514a6db735d3feabd325d34cc4574107419a
FilipusKevin <filipuskevin9@gmail.com>
2022-09-15 14:15:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10730/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10730
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari- hari untuk membuat hari makin Indah"),
+        Task("Cahpter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha Mama Yaemiko tericnta / Keging Wangi"),
+        Task("Chapter 3", "Ngopi", "Sehabis Nguli, Marilah Kita Ngopi"),
+        Task("Chapter 4", "Dimana Mama Ai?", "Buat Adu Mekanik"),
+        Task("Chapter 5", "Upload TikTok", "Buat Pamer ke view Showcase Ayaka Pyro C6")
+    )
+}
